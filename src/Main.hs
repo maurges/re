571209410ca975985dce5bbec9134b639fd19cce
morj <morj@morj.men>
2023-08-08 21:15:12 +0200

    Formatting and hlint

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DerivingVia #-}
 {-# LANGUAGE OverloadedLists #-}
 
@@ -19,35 +18,41 @@ import Data.Text.Encoding (decodeUtf8, encodeUtf8)
 import Network.HTTP.Types.Status (seeOther303, status404)
 import Network.Wai (Application, rawPathInfo, responseBuilder, responseLBS)
 import Network.Wai.Handler.Warp (runSettings)
-import System.Directory (XdgDirectory(XdgConfig), getXdgDirectory, makeAbsolute)
+import System.Directory
+    ( XdgDirectory (XdgConfig)
+    , getXdgDirectory
+    , makeAbsolute
+    )
 import System.Environment (getArgs, lookupEnv)
-import System.FSNotify (Event(Added, Modified), startManager, watchDir)
-import System.FilePath ((</>), splitFileName)
+import System.FSNotify (Event (Added, Modified), startManager, watchDir)
+import System.FilePath (splitFileName, (</>))
 
 data Settings = Settings
     { port :: !Int
     , redirectsPath :: !FilePath
-    } deriving (Eq, Show)
+    }
+    deriving (Eq, Show)
 
 envSettings :: IO Settings
 envSettings = do
     redirectsPathDef <- getXdgDirectory XdgConfig "re-server/redirects.ron"
     let portDef = 8040
-    port <- fromMaybe portDef . fmap read <$> lookupEnv "PORT"
+    port <- maybe portDef read <$> lookupEnv "PORT"
     redirectsPath <- fromMaybe redirectsPathDef <$> lookupEnv "REDIRECTS_FILE"
-    pure Settings {port, redirectsPath}
+    pure Settings{port, redirectsPath}
 
 parseCommandLine :: [String] -> Settings -> Settings
-parseCommandLine = go where
+parseCommandLine = go
+  where
     go [] !x = x
     go ("--port" : port : rest) !x =
-        go rest $! x {port = read port}
-    go ( ('-':'-':'p':'o':'r':'t':'=':port) : rest ) !x =
-        go rest $! x {port = read port}
+        go rest $! x{port = read port}
+    go (('-' : '-' : 'p' : 'o' : 'r' : 't' : '=' : port) : rest) !x =
+        go rest $! x{port = read port}
     go ("--config" : path : rest) !x =
-        go rest $! x {redirectsPath = path}
-    go ( ('-':'-':'c':'o':'n':'f':'i':'g':'=':path) : rest ) !x =
-        go rest $! x {redirectsPath = path}
+        go rest $! x{redirectsPath = path}
+    go (('-' : '-' : 'c' : 'o' : 'n' : 'f' : 'i' : 'g' : '=' : path) : rest) !x =
+        go rest $! x{redirectsPath = path}
     go (_noMatch : rest) !x = go rest x
 
 newtype Redirects = Redirects (Map Text Text)
@@ -62,7 +67,7 @@ app redirects req respond = do
     case Map.lookup path redirMap of
         Just redir ->
             let redir' = encodeUtf8 redir
-            in respond $ responseBuilder seeOther303 [("Location", redir')] mempty
+            in  respond $ responseBuilder seeOther303 [("Location", redir')] mempty
         Nothing -> respond $ responseLBS status404 [] "Not Found"
 
 reportError :: SomeException -> IO ()
@@ -70,7 +75,7 @@ reportError e = putStrLn $ "Caught error: " <> show e
 
 main :: IO ()
 main = do
-    appSettings@Settings {port, redirectsPath} <-
+    appSettings@Settings{port, redirectsPath} <-
         parseCommandLine <$> getArgs <*> envSettings
     putStrLn $ "App settings: " <> show appSettings
 
@@ -95,9 +100,10 @@ main = do
         Modified path _ _ | path == file || path == dir </> file -> reloadRedirects
         _ -> pure ()
 
-    let settings = Warp.defaultSettings
-            & Warp.setPort port
-            & Warp.setHost "127.0.0.1"
+    let settings =
+            Warp.defaultSettings
+                & Warp.setPort port
+                & Warp.setHost "127.0.0.1"
 
     putStrLn "Server starting..."
     runSettings settings $ app redirectsRef
